f427979af082c858322a202c519f174eb7c1be2c
SnowYoung <xyang.pan@gmail.com>
2023-04-25 15:12:34 +0800

    delete test

--- libs/openai-lib/src/lib/openai-lib.spec.ts
+++ libs/openai-lib/src/lib/openai-lib.spec.ts
@@ -1,12 +1,5 @@
 import { OpenAILib } from './openai-lib';
 
 describe('openaiLib', () => {
-  it('should work', () => {
-    const openai =  new OpenAILib('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','https://openai.notfound404.info/v1')
-
-   return openai.chat([{role: 'user', content: 'Hi'}], true).then((res: any)=> {
-    console.info(res)
-     
-   })
-  },50000);
+ 
 });
