6168e29d2114c910d8df9595584df22e1e6ca6de
liuxin <454875315@qq.com>
2023-04-28 11:41:27 +0800

    fix

--- apps/openai/src/app/views/Chat/index.tsx
+++ apps/openai/src/app/views/Chat/index.tsx
@@ -591,6 +591,7 @@ export function Chat() {
                         >
                           <Box
                             className="chat-wrap"
+                            width={'100%'}
                             bgcolor={chats[virtualRow.index].role === "user" ? "#e7f8ff" : "rgba(0, 0, 0, .05)"}
                             border="1px solid #dedede"
                             borderRadius="10px"
