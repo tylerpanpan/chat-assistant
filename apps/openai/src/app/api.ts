da4161e7b0618927fec653fd702ea008437e52ab
liuxin <454875315@qq.com>
2023-04-28 19:06:41 +0800

    多会话功能

--- apps/openai/src/app/api.ts
+++ apps/openai/src/app/api.ts
@@ -154,7 +154,18 @@ export class ChatAPI extends BaseAPI {
       method: 'POST',
       body: JSON.stringify({ text, stream })
     })
-    // return this.post<any>(`/api/chat/${id}`, { text, stream })
+  }
+
+  getCharacterChat(characterId: any) {
+    return this.get<any>('/api/chat', { characterId })
+  }
+
+  createChat(characterId: any) {
+    return this.post<any>('/api/chat', { characterId })
+  }
+
+  delChat(id: number) {
+    return this.delete<any>(`/api/chat/${id}`, {})
   }
 
   clearMessage(id: string) {
