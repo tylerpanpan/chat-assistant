d93a03c43d6c1c64e5959cc5a44591ca25221e45
谢立波 <liboy_houdi@163.com>
2023-05-04 20:49:29 +0800

    delete deprecated file

--- apps/openai-api/src/app/apiKeyPool/apiKeyPool.module.ts
+++ apps/openai-api/src/app/apiKeyPool/apiKeyPool.module.ts
@@ -2,14 +2,12 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Global, Module } from "@nestjs/common";
 import { ApiKeyPool } from "./apiKeyPool.entity";
 import { ApiKeyPoolService } from "./apiKeyPool.service";
-import { ApiKeyPoolController } from "./apiKeyPool.controller";
 
 @Global()
 @Module({
   imports: [
     MikroOrmModule.forFeature([ApiKeyPool])
   ],
-  controllers: [ApiKeyPoolController],
   providers: [ApiKeyPoolService],
   exports: [ApiKeyPoolService]
 })
